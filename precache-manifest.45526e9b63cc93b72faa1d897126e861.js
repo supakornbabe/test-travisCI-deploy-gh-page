86ed544af9e03078cf4ef3ddbd193a6ded2c497f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-27 15:23:32 +0000

    Deploy supakornbabe/test-travisCI-deploy-gh-page to github.com/supakornbabe/test-travisCI-deploy-gh-page.git:gh-pages

---
+++ precache-manifest.45526e9b63cc93b72faa1d897126e861.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c12a6deb74374eb769e",
+    "url": "/me/static/css/main.55fc502a.chunk.css"
+  },
+  {
+    "revision": "0c12a6deb74374eb769e",
+    "url": "/me/static/js/main.0c12a6de.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "65084068233d8dd6bfe83454eb19fd89",
+    "url": "/me/index.html"
+  }
+];
